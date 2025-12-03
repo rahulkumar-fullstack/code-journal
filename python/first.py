669202b84e2e6f21cbdd5d66d93bab1466d1cba5
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2025-12-03 14:33:03 +0530

    first python code
    Starting ....

---
+++ python/first.py
@@ -0,0 +1,19 @@
+print("Hello World")
+#################################
+
+a = input("Enter First Name: ")
+b = input("Enter Last Name: ")
+
+print(a+" "+b)
+
+
+
+'''
+#Output:
+Hello World
+Enter First Name: a
+Enter Last Name: b
+a b
+
+'''
+
