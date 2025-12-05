ddeb677bb17feb9276dd2586bfc802a050e7b06f
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2025-12-05 10:23:07 +0530

    Add #3
    Printing table from 1 to 20 using loop

--- python/problem1.py
+++ python/problem1.py
@@ -1,15 +1,22 @@
-'''
-#1 Printing the table of 5
-n = 5
-for j in range(1,11):
-    print(f"{n} x {j} = {j*n}")
+#3 Printing the Table from 1 to 20
 
-    
+for a in range(1,21):
+    print(f"Table of {a} :")
+    for b in range(1,11):
+       print(f"| {a} x {b} = {a*b} |")
+ 
+
+'''
 #2 Printing the Table of your Choice
 a = int(input("Enter a Number: "))
 for b in range(1,11):
     print(f"| {a} x {b} = {a*b} |")
 
+
+#1 Printing the table of 5
+n = 5
+for j in range(1,11):
+    print(f"{n} x {j} = {j*n}")
+
 '''
 
-#3 
