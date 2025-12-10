d884af2a8911ffa73d607277a89e865d1b430f95
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2025-12-10 14:59:32 +0530

    #1 variables

---
+++ python/basics/variables.py
@@ -0,0 +1,43 @@
+'''
+#1 Basic Variable assign
+
+x = 10          # integer
+name = "Rahul"  # string
+pi = 3.14       # float
+is_active = True  # boolean
+
+print(x, name, pi, is_active)
+
+#-----------------------------------
+
+#2 Multi variable assign
+
+a, b, c ,name = 10, 20, 30, "Rahul"
+print(a,b,c, name) #10 20 30 Rahul
+
+#-----------------------------------
+
+#3 assign same value to multiple var
+
+x = y = z = 100
+print(x,y,z)  #100 100 100
+
+#-----------------------------------
+
+#4 assign thorugh input
+
+num = int(input("Enter number: "))
+price = float(input("Enter price: "))
+print(num,price) #12 1.3
+
+#------------------------------------
+
+#5 Dynamic Assignment
+
+x = 10
+x = "Now I'm a string"
+print(x) #Now I'm a string
+
+#------------------------------------
+'''
+#6 
