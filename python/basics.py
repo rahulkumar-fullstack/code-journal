da33ac787b4301409d99ca716e1b1908e3f54fcb
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2025-12-11 10:45:36 +0530

    Add Python Indentation

--- python/basics.py
+++ python/basics.py
@@ -1,1 +1,14 @@
 # Basics of Python -  Quick
+print("Hello, World!")
+
+## Indentation
+#-> refers to the spaces at the beginning of a code line
+if 5 > 1:
+  print("Five")
+
+''' Error
+if 5>1:
+print("five")
+'''
+
+#
