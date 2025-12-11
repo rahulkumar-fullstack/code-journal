ac7f3dee8b36dc236f7e4fc154d99128cdf107de
Rahulkumar Gupta <159034627+rahulkumar-fullstack@users.noreply.github.com>
2025-12-11 11:14:48 +0530

    Add statemets, Variable
    Removed errors and added explanations for statements and variables.

--- python/basics.py
+++ python/basics.py
@@ -6,9 +6,46 @@ print("Hello, World!")
 if 5 > 1:
   print("Five")
 
-''' Error
+''' 
+#Error -1
 if 5>1:
 print("five")
+
+#Error -2
+if 5>1:
+ print("five")
+     print("five")
 '''
 
-#
+## Statements
+#-> programming instructions are called statements
+print("Hello World!")
+print('Have a good day')
+
+print("a"); print('b')
+
+'''
+#error
+print("Python is fun!") print("Really!")
+'''
+
+## Variables
+#-> containers for storing data values
+b = "Apple";print(b)
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+###note--double quotes are the same as single quotes
+
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+###note--Variable names are case-sensitive
+
+## 
+
+
+
+
